7c29dbf895d30b8bc51a42b7730d9675d6e3eb56
Mark <marknjensen@outlook.com>
2017-09-09 22:21:30 +0200

    Added junit

---
+++ Snake/src/Game.java
@@ -0,0 +1,6 @@
+/**
+ * Created by markn on 09-09-2017.
+ */
+public interface Game {
+
+}
---
+++ Snake/tests/GameTest.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by markn on 09-09-2017.
+ */
+public class GameTest {
+
+    @Test
+    public void JunitTest() {
+        assertEquals(true, true);
+    }
+}
